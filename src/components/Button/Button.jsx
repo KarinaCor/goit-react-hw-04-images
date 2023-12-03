26f5542a49e73a83a71782c3a0e4cabdd70ae677
KarinaCor <mksmvkarina@gmail.com>
2023-12-03 21:35:14 +0200

    commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import * as SC from '../Button/Button.styled'
 export const Button = ({onClick}) => {
     return(
 <SC.Button type="button" onClick={onClick}>
-       Load More 
+       Load More!
     </SC.Button>
     )
     
